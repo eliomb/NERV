841533836afaa821fdd60d2d6fcd57032aff50d4
elio mendez <eliojose2003@hotmail.com>
2021-04-04 11:55:07 -0300

    documento el 2)g). sorry :D

--- 2)g).py
+++ 2)g).py
@@ -1,6 +1,7 @@
 import math
 
 def pitagoras():
+    """esta funcion utiliza el teorema de pitagoras y el modulo de math que me permite utilizar variadas funciones matematicas. bendito sea el python"""
     a = float(input("Valor del cateto A: "))
     b = float(input("Valor del cateto B: "))
     if b and a > 0:
