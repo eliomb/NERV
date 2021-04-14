11b42774e04df78d684afa7d40baf38717fff375
eliomb <eliojose2003@hotmail.com>
2021-04-14 20:55:56 -0300

    guardo el 6)

---
+++ 6).py
@@ -0,0 +1,18 @@
+def cuatronumeros():
+    numeros = []
+    
+    for i in range(0, 4):
+        num = int(input())
+        numeros.append(num)
+
+    for x in range(0, 4)
+        for y in range(0, 4)
+            numeros[x, y]
+        
+    numeros = [i * 2 for i in numeros]
+    print(numeros)
+    
+
+
+
+cuatronumeros()
