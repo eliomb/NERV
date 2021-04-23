bb6fe304f423aaa28430f97562a53da81f7e1257
elio mendez <eliojose2003@hotmail.com>
2021-04-23 17:31:04 -0300

    documento el 6)

--- 6).py
+++ 6).py
@@ -1,15 +1,28 @@
-def multiplicacion(numero):
-    return numero * (numero + 1)
-        
-    
+
 numeros = []
+def multiplicacion():
+    """esta funcion debe pedir cuatro numeros, y de entre los cuatro, multiplicar dos de ellos y ver cual es el mayor resultado de la multiplicacion de entre dos de los numeros"""
+    for i in range(0, 4):
+            num = int(input("Ingrese cuatro numeros: "))
+            numeros.append(num)
+    mayor = 0
+    resultado = 0
+    for i in range(0, 4):
+        print(numeros[i])
+        a = numeros[i]
+    """aca hay un doble for. mientras i esta parado en un numero de la lista, el segundo for va a pasar por el resto de los numeros de la lista, y a dentro validamos que ambos numeros no sean los mismos a si no se multipliquen entre si."""
+        for x in range(0, 4):
+            resultado = abs(a * numeros[x])
+            if resultado > mayor and numeros[i] != numeros[x]:
+                mayor = resultado
 
-for i in range(0, 4):
-        num = int(input("Ingrese cuatro numeros: "))
-        numeros.append(num)
+    print("El mayor valor es: ", mayor)
+
+"""print(list(map(multiplicacion, numeros)))"""
+
+multiplicacion()
 
 
-print(list(map(multiplicacion, numeros)))
 
 
 
