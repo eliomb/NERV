5a4dd754d4f38c4d04ccfcb0f78851cf2a8ccdc2
eliomb <eliojose2003@hotmail.com>
2021-04-24 12:44:46 -0300

    rehago el 7)d), lei mal el punto.

--- 7)d).py
+++ 7)d).py
@@ -1,20 +1,49 @@
 import datetime
 from calendar import monthrange
 
+def fechavalida():
+    date = input("Ingresa una fecha valida (DD/MM/AAAA ): ")
+
+    dia,mes,año = date.split('/')
+
+    valida = True
+    try:
+        datetime.datetime(int(año),int(mes),int(dia))
+    except ValueError:
+        valida = False
+
+    if valida == True:
+        print("La fecha {} es valida.".format(date))
+        return date
+    else:
+        print("La fecha es invalida, o los datos ingresados no son fechas.")
+        exit
+
 
 def findemes():
-    """nuevamente uso las librerias calendar y datetime para hacer un programa que mide los dias que faltan hasta fin de mes utilizando "monthrange" de calendar. esto basicamente nos dice la semana en la que estamos y los dias totales del mes, y usando ese valor, guardo en una variable los dias totales del mes actual, y en otra variable guardo el dia actual. luego, para evitar errores, utilizo un try-except para meter una cuenta rapida, y automaticamente hace el calculo sin necesidad de algun input"""
-    fecha = datetime.datetime.now()
-    yearnow = fecha.year
-    monthnow = fecha.month
-    daynow = fecha.day
+    """
+    en este codigo utilizo una funcion de un ejercicio anterior en el que valido primero
+    si la fecha elegida es valida, y luego regresa dicha fecha al terminar la funcion.
+    esta misma fecha luego la guardo en una variable en findemes() y luego la convierto
+    a datetime con "strptime", para luego guardar en variables cada parte de la fecha que luego
+    me sera util para el monthrange, que mide la semana del mes y la cantidad de dias en el mes
+    elegido. de ambos valores, solo elijo el de los dias totales en el mes, asi en un try-except
+    hago una cuenta rapida.
+    """
+    date = fechavalida()
+    formato = "%d/%m/%Y"
+    dateobj = datetime.datetime.strptime(date, formato)
+    yearnow = dateobj.year
+    monthnow = dateobj.month
+    daynow = dateobj.day
     days = monthrange(yearnow, monthnow)[1]
     try:
         daysleft = days - daynow
         print("Faltan {} dias para el fin de mes.".format(daysleft))
     except ValueError:
         print("Error de formato.")
-    
 
 
 findemes()
+    
+
