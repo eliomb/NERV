b22234049dc188e71cb47e51357e3dec04de1f4f
elio mendez <eliojose2003@hotmail.com>
2021-04-25 17:27:19 -0300

    termino el 7) con el 7)g)

--- 7)g).py
+++ 7)g).py
@@ -1,5 +1,14 @@
 from calendar import monthrange
+from datetime import date
+from dateutil import relativedelta
 import datetime
+
+"""
+    usando la libreria dateutil para sacar relativedelta,
+    simplemente medi la diferencia entre ambas fechas ingresadas
+    por medio de un dato de tipo delta, que contiene la diferencia
+    de tiempo entre las dos fechas.
+"""
 
 def fechavalida():
     date1 = input("Ingresa una fecha valida (DD/MM/AAAA ): ")
@@ -22,4 +31,27 @@ def fechavalida():
         print("La fecha es invalida, o los datos ingresados no son fechas.")
         exit
 
-fechavalida()
+def diasaño():
+    date1,date2 = fechavalida()
+    formato = "%d/%m/%Y"
+    fecha1 = datetime.datetime.strptime(date1, formato)
+    fecha2 = datetime.datetime.strptime(date2, formato)
+    try:
+        diferencia = relativedelta.relativedelta(fecha1,fecha2)
+        años = diferencia.years
+        meses = diferencia.months
+        dias = diferencia.days
+        print("Hay {} años, {} meses y {} dias entre las fechas.".format(años,meses,dias))
+    except ValueError:
+        print("Error de formato.")
+    
+
+
+
+
+
+
+
+
+
+diasaño()
